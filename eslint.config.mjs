6ba995674bb112f7cfb995e685748dbbef4be2a8
LeeYunHwan <cyzhqly@gmail.com>
2025-07-12 00:10:15 +0900

    chore: ESLint 및 Prettier 설정 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,8 +12,24 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...storybook.configs["flat/recommended"]
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...storybook.configs['flat/recommended'],
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          disallowTypeAnnotations: false,
+        },
+      ],
+      // Prettier와 충돌하는 스타일 규칙 끄기
+      quotes: 'off',
+      semi: 'off',
+      'comma-dangle': 'off',
+      'object-curly-spacing': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
