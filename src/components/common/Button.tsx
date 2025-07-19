8a781dfa549192bf1f6c0e02280a0b169521b86c
LeeYunHwan <cyzhqly@gmail.com>
2025-07-19 22:16:24 +0900

    refactor: 확장성을 고려한 방식으로 Button 컴포넌트 리팩토링

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,60 +2,27 @@ import { Button as ShadcnButton } from '@/lib/shadcn/components/ui/button';
 import { cn } from '@/lib/shadcn/utils';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'default' | 'round' | 'square';
-  size?: 'md' | 'icon-sm' | 'icon-md';
   isActive?: boolean;
   disabled?: boolean;
   children: React.ReactNode;
 }
 
 export default function Button({
-  variant = 'default',
-  size = 'md',
   isActive = false,
   disabled = false,
   children,
   className,
   ...props
 }: ButtonProps) {
-  const iconBtn = variant === 'round' || variant === 'square';
-  const iconBtnStyle = 'bg-nt-primary-50 aspect-square';
-
   const baseStyles = cn(
-    'flex p-0 bg-nt-primary text-nt-neutral-white hover:bg-nt-primary-400 cursor-pointer',
+    'flex p-0 w-full h-[46px] bg-nt-primary text-nt-neutral-white hover:bg-nt-primary-400 cursor-pointer',
     'active:bg-nt-primary-600 disabled:opacity-100 disabled:bg-nt-neutral-200 disabled:text-nt-neutral-400',
     'focus-visible:border focus-visible:border-nt-primary focus-visible:ring-0 focus-visible:text-nt-primary focus-visible:bg-nt-neutral-white',
     isActive && !disabled && 'bg-nt-primary-100 text-nt-primary-600 border border-nt-primary-600',
-    iconBtn && iconBtnStyle,
   );
 
-  const getVariantStyles = () => {
-    switch (variant) {
-      case 'round': // 버튼-원형
-        return 'rounded-full';
-      case 'default': // 기본 버튼
-      case 'square': // 버튼-사각형
-        return 'rounded-nt-radius';
-    }
-  };
-
-  const getSizeStyles = () => {
-    switch (size) {
-      case 'md': // 기본 버튼
-        return 'h-[var(--nt-btn-h)] w-[343px]';
-      case 'icon-sm': // 소셜 로그인(원형)
-        return 'h-[50px]';
-      case 'icon-md': // 버튼-원형 & 버튼-사각형
-        return 'h-[60px]';
-    }
-  };
-
   return (
-    <ShadcnButton
-      className={cn(baseStyles, getVariantStyles(), getSizeStyles(), className)}
-      disabled={disabled}
-      {...props}
-    >
+    <ShadcnButton className={cn(baseStyles, className)} disabled={disabled} {...props}>
       {children}
     </ShadcnButton>
   );
