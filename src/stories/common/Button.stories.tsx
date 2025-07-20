80bc2fc9399f764249994330a2f845fcb8692e4e
LeeYunHwan <cyzhqly@gmail.com>
2025-07-20 14:29:25 +0900

    refactor: Button 스토리북 파일 경로 이동

--- src/stories/common/Button.stories.tsx
+++ src/stories/common/Button.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import Button from '@/components/common/Button';
 import Image from 'next/image';
-import icon from '../../public/images/ex_icon.png';
+import icon from '../../../public/images/ex_icon.png';
 
 const meta: Meta<typeof Button> = {
   title: 'design-system/Button',
