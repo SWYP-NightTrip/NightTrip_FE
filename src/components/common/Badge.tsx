cc65718dceed2efa79c8dec039380c40a56c193f
LeeYunHwan <cyzhqly@gmail.com>
2025-07-21 00:51:37 +0900

    feat: 추가적인 HTML div 속성을 받을 수 있도록 확장

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -1,13 +1,16 @@
 import { cn } from '@/lib/shadcn/utils';
 
-type BadgeProps = {
+type BadgeProps = React.HTMLAttributes<HTMLDivElement> & {
   children: React.ReactNode;
-  className?: string;
 };
 
-export default function Badge({ children, className }: BadgeProps) {
+export default function Badge({ children, className, ...props }: BadgeProps) {
   const badgeClass =
     'flex items-center justify-center w-[53px] h-[28px] rounded-full bg-nt-neutral-white border border-nt-primary font-medium text-xs';
 
-  return <div className={cn(badgeClass, className)}>{children}</div>;
+  return (
+    <div className={cn(badgeClass, className)} {...props}>
+      {children}
+    </div>
+  );
 }
