5c79a065e89af1ac0703f2d39ae5cf7223c51081
LeeYunHwan <cyzhqly@gmail.com>
2025-07-22 00:30:48 +0900

    fix: 외부에서 rounded 클래스 오버라이드 시 기본값 제거

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -7,10 +7,11 @@ export default function Input({
   ...props
 }: React.ComponentProps<typeof ShadcnInput>) {
   const baseStyles = cn(
-    'border border-nt-neutral-250 rounded-nt-radius shadow-none px-[16px] py-[13px] h-[46px]',
+    'border border-nt-neutral-250 shadow-none px-[16px] py-[13px] h-[46px]',
     'focus-visible:border-nt-primary focus-visible:ring-transparent focus-visible:ring-0',
     'disabled:border-nt-neutral-400 disabled:bg-nt-neutral-200 disabled:placeholder-nt-neutral-400 disabled:opacity-100',
     'placeholder:text-nt-neutral-250',
+    !className?.includes('rounded-') && 'rounded-nt-radius'
   );
 
   return <ShadcnInput type={type} className={cn(baseStyles, className)} {...props} />;
