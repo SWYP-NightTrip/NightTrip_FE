00c0ef2d3e7c37c3c08d6dd0f0920986785fde89
LeeYunHwan <cyzhqly@gmail.com>
2025-07-24 10:46:51 +0900

    feat: 소셜 로그인 아이콘 추가 및 적용

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -4,7 +4,10 @@ import Link from 'next/link';
 import Rabbit from '@/icons/rabbit.svg';
 import LoginLogo from '@/icons/login-logo.svg';
 import Header from '@/pages/LoginPage/ui/header';
-
+import Naver from '@/icons/naver.svg';
+import Google from '@/icons/google.svg';
+import Apple from '@/icons/apple.svg';
+import Kakao from '@/icons/kakao.svg';
 const socialLoginBtnClass = 'rounded-full w-[50px] h-[50px] border border-nt-neutral-250';
 
 export default function LoginPage() {
@@ -15,11 +18,11 @@ export default function LoginPage() {
       <div className="flex flex-col w-full p-3">
         <div className="flex flex-col w-full gap-4 items-center">
           <p className="header2">밤에 떠나는 여행</p>
-          <LoginLogo/>
+          <LoginLogo />
         </div>
       </div>
 
-      <Rabbit/>
+      <Rabbit />
 
       <div className="flex mb-2 mt-7">
         <div className="border-[0.5px] w-[80px] my-auto border-nt-neutral-250" />
@@ -29,19 +32,19 @@ export default function LoginPage() {
 
       <div className="flex gap-4">
         <LoginButton provider="kakao" className={cn(socialLoginBtnClass, 'bg-[#FEE500]')}>
-          K
+          <Kakao />
         </LoginButton>
         <LoginButton provider="naver" className={cn(socialLoginBtnClass, 'bg-[#03C75A]')}>
-          N
+          <Naver />
         </LoginButton>
         <LoginButton
           provider="google"
           className={cn(socialLoginBtnClass, 'bg-[#FFFFFF] text-nt-neutral-black')}
         >
-          G
+          <Google />
         </LoginButton>
         <LoginButton provider="apple" className={cn(socialLoginBtnClass, 'bg-[#000000]')}>
-          A
+          <Apple className='-translate-y-0.5'/>
         </LoginButton>
       </div>
 
