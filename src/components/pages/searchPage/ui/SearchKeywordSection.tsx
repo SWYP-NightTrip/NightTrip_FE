402e2be9093d3d8c6a4c1a46ad26d8bc4f4c65d4
turtle601 <turtle601@naver.com>
2025-07-26 11:12:51 +0900

    feat: 검색 페이지 UI 구현

---
+++ src/components/pages/searchPage/ui/SearchKeywordSection.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+interface SearchKeyword {
+  id: number;
+  name: string;
+  url: string;
+}
+
+interface SearchKeywordSectionProps {
+  title: string;
+  keywords: SearchKeyword[];
+}
+
+export default function SearchKeywordSection({ title, keywords }: SearchKeywordSectionProps) {
+  return (
+    <article>
+      <h3 className="text-lg font-semibold text-gray-900 mb-[14px]">{title}</h3>
+      <div className="flex flex-wrap gap-x-2 gap-y-3">
+        {keywords.map(keyword => (
+          <Link
+            href={keyword.url}
+            key={keyword.id}
+            className="px-4 py-2 flex items-center justify-center rounded-full bg-[#DEDEDE]"
+          >
+            <span className="text-sm text-[#5c5c5c] font-normal text-center leading-[20px] tracking-[0.1px]">
+              {keyword.name}
+            </span>
+          </Link>
+        ))}
+      </div>
+    </article>
+  );
+}
