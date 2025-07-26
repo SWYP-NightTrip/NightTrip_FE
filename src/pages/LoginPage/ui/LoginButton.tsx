e0ab9e113c88c5a61a6d2a8923bddd8626eadf82
LeeYunHwan <cyzhqly@gmail.com>
2025-07-26 09:35:03 +0900

    feat: OAuth 로그인 api 연동

--- src/pages/LoginPage/ui/LoginButton.tsx
+++ src/pages/LoginPage/ui/LoginButton.tsx
@@ -2,6 +2,7 @@
 
 import Button from '@/components/common/Button';
 import { cn } from '@/lib/shadcn/utils';
+import { useRouter } from 'next/navigation'; // next/navigation에서 useRouter를 임포트
 
 interface LoginButtonProps {
   provider: 'kakao' | 'naver' | 'google' | 'apple';
@@ -11,11 +12,13 @@ interface LoginButtonProps {
 
 const socialLoginBtnClass = 'rounded-full w-[50px] h-[50px] border border-nt-neutral-250';
 
-const handleLogin = (provider: LoginButtonProps['provider']) => {
-  console.log(`${provider} 로그인 클릭!`);
-};
-
 export default function LoginButton({ provider, children, className }: LoginButtonProps) {
+  const router = useRouter();
+
+  const handleLogin = (provider: LoginButtonProps['provider']) => {
+    router.push(`https://dev.nighttrip.co.kr/oauth2/authorization/${provider}`);
+  };
+
   return (
     <Button className={cn(socialLoginBtnClass, className)} onClick={() => handleLogin(provider)}>
       {children}
