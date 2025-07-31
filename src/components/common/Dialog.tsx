f92a76760b1e24bf4a64baed4f901dfc6b436ac3
LeeYunHwan <cyzhqly@gmail.com>
2025-07-31 21:21:54 +0900

    style: Dialog 최소 높이 추가 및 height 값 조절 시 위로 길어지도록 수정

--- src/components/common/Dialog.tsx
+++ src/components/common/Dialog.tsx
@@ -19,7 +19,7 @@ function DialogContent({
   return (
     <ShadcnDialogContent
       className={cn(
-        'flex flex-col px-4 py-3 gap-0 !max-w-[375px] bottom-0 translate-y-0 rounded-t-[40px] rounded-b-none data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom',
+        'flex flex-col px-4 py-3 gap-0 !max-w-[375px] min-h-[400px] bottom-0 top-auto translate-y-0 rounded-t-[40px] rounded-b-none data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom',
         className,
       )}
       showCloseButton={showCloseButton}
