bd19fd7f5c59c274a326bfa3f8ee0a1feeb21d97
turtle601 <turtle601@naver.com>
2025-08-06 09:58:41 +0900

    feat: 에러 관련 객체 및 request 함수 구현

---
+++ src/utils/request/reqeust.ts
@@ -0,0 +1,40 @@
+import {
+  HTTP401Error,
+  HTTP500Error,
+  HTTPEtcError,
+  UnExpectedAPIError,
+} from '@/utils/request/error';
+
+interface ReqParams {
+  url: string;
+  params?: URLSearchParams;
+  options?: RequestInit;
+}
+
+export const requestAPI = <T>({ url, params, options }: ReqParams): Promise<T> => {
+  const fullUrl = params ? `${url}?${params.toString()}` : `${url}`;
+  return new Promise((resolve, reject) => {
+    fetch(fullUrl, options)
+      .then(response => {
+        if (!response.ok) {
+          switch (response.status) {
+            case 401:
+              reject(new HTTP401Error());
+              return;
+            case 500:
+              reject(new HTTP500Error());
+              return;
+            default:
+              reject(new HTTPEtcError(response.status));
+              return;
+          }
+        }
+
+        return response.json();
+      })
+      .then((data: T) => resolve(data))
+      .catch(error => {
+        reject(new UnExpectedAPIError(error.message));
+      });
+  });
+};
