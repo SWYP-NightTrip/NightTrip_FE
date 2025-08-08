ab98e59933b4b921069153dc2b2647952d16e2e4
LeeYunHwan <cyzhqly@gmail.com>
2025-08-08 20:29:41 +0900

    fix: 마이페이지 프로필 이미지 fallback 수정 및 라우팅 구현

--- src/components/pages/MyPage/profile/index.tsx
+++ src/components/pages/MyPage/profile/index.tsx
@@ -11,11 +11,11 @@ export default function Profile() {
     <>
       <Header />
       <div className="flex justify-center items-center h-[180px]">
-        <Image src={data?.data.userAvatarUrl ?? '/images/fallback/tripcard_fallback.png'} alt="캐릭터 이미지" width={188} height={180} />
+        <Image src={data?.data.userAvatarUrl ?? '/images/fallback/profile_fallback.png'} alt="캐릭터 이미지" width={188} height={180} />
       </div>
       <div className="flex flex-col justify-center items-center gap-4 px-[16px] py-[10px]">
         <p className="header1 text-nt-neutral-white">{data?.data.userName}</p>
-        <Link href="#">
+        <Link href={`/my/likes`}>
           <Badge className="bg-nt-secondary-500 text-md text-nt-neutral-white border-0 w-[200px] h-[33px] active:bg-nt-secondary-600 active:text-nt-neutral-white hover:bg-nt-secondary-100 hover:text-nt-neutral-400">
             좋아요 갯수&nbsp;
             <span className="text-nt-primary font-semibold">{data?.data.likedSpotsCount}</span>개
--- src/components/pages/MyPage/ui/header.tsx
+++ src/components/pages/MyPage/ui/header.tsx
@@ -1,21 +1,29 @@
 import Button from '@/components/common/Button';
 import TopNav from '@/components/common/TopNav';
-import Link from 'next/link';
 import Back from '@/icons/back.svg';
+import { useRouter } from 'next/navigation';
 // import Setting from '@/icons/setting.svg';
 
 export default function Header() {
+  const router = useRouter();
+
+  const handleBackClick = () => {
+    if (window.history.length > 1) {
+      router.back();
+    } else {
+      router.push('/');
+    }
+  };
+
   return (
-    <TopNav className='bg-nt-neutral-white mb-10'>
-      <Link href="#">
-        <Button className="w-[42px] h-[42px] bg-transparent shadow-none">
-          <Back />
-        </Button>
-      </Link>
+    <TopNav className="bg-nt-neutral-white mb-10">
+      <Button className="w-[42px] h-[42px] bg-transparent shadow-none" onClick={handleBackClick}>
+        <Back />
+      </Button>
 
-      <p className='mx-auto header2'>마이페이지</p>
+      <p className="mx-auto header2">마이페이지</p>
 
-      <div className='w-[42px]'/>
+      <div className="w-[42px]" />
       {/* 추후 설정 페이지 생성 시 적용 */}
       {/* <Link href="#" className='flex flex-row-reverse'>
         <Button className="w-[42px] h-[42px] bg-transparent shadow-none">
