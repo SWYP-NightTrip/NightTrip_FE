3b4ceeb599d38714ebf9f44e7db3e5a18cca6322
turtle601 <turtle601@naver.com>
2025-08-10 13:33:50 +0900

    fix: 하단 푸터 디자인 수정

--- src/components/pages/MainPage/ui/header.tsx
+++ src/components/pages/MainPage/ui/header.tsx
@@ -1,18 +1,12 @@
 import TopNav from '@/components/common/TopNav';
 
 import LogoIcon from '@/icons/logo.svg';
-import AddTravelIcon from '@/icons/add-travel.svg';
 
 export default function Header() {
   return (
     <TopNav>
       <div className="w-full h-full header1 flex items-center justify-start pl-4">
         <LogoIcon />
-      </div>
-      <div className="w-full h-full flex items-center justify-end pr-2">
-        <button className="bg-nt-white active:bg-nt-primary-50 p-2 rounded-nt-radius">
-          <AddTravelIcon />
-        </button>
       </div>
     </TopNav>
   );
