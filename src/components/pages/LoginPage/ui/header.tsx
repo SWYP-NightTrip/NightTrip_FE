d50dcc44d320b9562ce1514d04e8e95847ef7a93
LeeYunHwan <cyzhqly@gmail.com>
2025-08-10 21:03:13 +0900

    fix: 로그인 페이지 백 라우팅 useRouter로 변경

--- src/components/pages/LoginPage/ui/header.tsx
+++ src/components/pages/LoginPage/ui/header.tsx
@@ -1,16 +1,29 @@
+'use client';
+
 import Button from '@/components/common/Button';
 import TopNav from '@/components/common/TopNav';
 
 import Back from '@/icons/back.svg';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Header() {
+  const router = useRouter();
+
+  const handleBackClick = () => {
+    if (window.history.length > 1) {
+      router.back();
+    } else {
+      router.push('/');
+    }
+  };
+
   return (
     <TopNav>
-      <Button className="w-[42px] h-[42px] bg-transparent shadow-none text-black">
-        <Link href="/">
-          <Back />
-        </Link>
+      <Button
+        className="w-[42px] h-[42px] bg-transparent shadow-none text-black"
+        onClick={handleBackClick}
+      >
+        <Back />
       </Button>
     </TopNav>
   );
