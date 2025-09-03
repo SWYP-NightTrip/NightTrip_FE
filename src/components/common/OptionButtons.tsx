d0291d57572d7d2eee3ad33534485531d35e4916
LeeYunHwan <cyzhqly@gmail.com>
2025-09-03 18:07:48 +0900

    style: OptionButtons 컴포넌트 상태별 style 수정

--- src/components/common/OptionButtons.tsx
+++ src/components/common/OptionButtons.tsx
@@ -16,15 +16,21 @@ interface OptionButtonsProps {
 
 export default function OptionButtons({ options, value, onChange, className }: OptionButtonsProps) {
   return (
-    <RadioGroup value={value} onValueChange={onChange} className={cn('flex flex-wrap gap-5', className)}>
+    <RadioGroup
+      value={value}
+      onValueChange={onChange}
+      className={cn('flex flex-wrap gap-5', className)}
+    >
       {options.map(option => (
         <div key={option.value} className="flex items-center">
           <RadioGroupItem value={option.value} id={option.value} className="sr-only" />
           <Label
             htmlFor={option.value}
             className={cn(
-              'px-4 py-2 h-[42px] rounded-[10px] header5 cursor-pointer transition-colors hover:bg-nt-neutral-300',
-              value === option.value ? 'bg-nt-neutral-dark-gray text-nt-neutral-white' : 'bg-nt-neutral-250 text-nt-neutral-400',
+              'px-4 py-2 h-[42px] rounded-[10px] header5 cursor-pointer transition-colors hover:bg-nt-neutral-100 hover:text-nt-neutral-400 text-nt-neutral-400',
+              value === option.value
+                ? 'bg-[#c8c7f0] border-1 border-nt-primary-400 text-nt-neutral-white'
+                : 'bg-nt-neutral-250',
             )}
           >
             {option.label}
